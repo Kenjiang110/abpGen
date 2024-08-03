9777ce650082c784622eeb71e9bc75c512ca3c79
蒋冠雄 <ken_yhy@163.com>
2024-08-03 18:57:09 +0800

    Fix: Menu template, GroupMenuItem's DisplayName

--- Bob.Abp.AppGen/Templates/Web/Menu/ContributorModify_Main.vue
+++ Bob.Abp.AppGen/Templates/Web/Menu/ContributorModify_Main.vue
@@ -4,5 +4,6 @@
         var {{ToCamel ModuleName}}MenuItem = context.Menu.GetMenuItem({{ModuleName}}Menus.Home);
         var l = context.GetLocalizer<{{ModuleName}}Resource>();
 
+
         return Task.CompletedTask;
     }
--- Bob.Abp.AppGen/Templates/Web/Menu/Contributor_Main.vue
+++ Bob.Abp.AppGen/Templates/Web/Menu/Contributor_Main.vue
@@ -1,4 +1,4 @@
 ﻿{{#EntityGroupChain}}
-    private readonly MenuItemInfo = dn{{Name}} = new({{ModuleName}}Menus.{{Name}}MenuGroupName, "Menu:{{ModuleName}}")
+    private readonly MenuItemInfo = dn{{Name}} = new({{ModuleName}}Menus.{{Name}}MenuGroupName, "Menu:{{Name}}")
 {{/EntityGroupChain}}
     private readonly MenuItemInfo = dn{{EntityName}} = new({{ModuleName}}Menus.{{EntityName}}, "Menu:{{EntityName}}", {{ModuleName}}Permissions.{{EntityName}}.Default, "~/{{LastEntityGroup.RelativePath}}/{{EntityName}}")
